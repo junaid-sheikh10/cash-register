fad0a646d5c78616c35088400e8adc60359d58f8
Junaid Sheikh <81684239+junaid-sheikh10@users.noreply.github.com>
2021-09-30 00:53:05 +0530

    final commit

--- main.js
+++ main.js
@@ -1,46 +1,57 @@
 var bill=document.querySelector("#billamount");
 var cash=document.querySelector("#cashgiven");
 var button=document.querySelector("#btn");
-var notes=document.querySelector("#noofNotes"); 
+const notes=document.querySelectorAll(".noofNotes");
+var out=document.querySelector("#msg");
 
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 
 button.addEventListener("click",clickHandler)
 
+out.style.display="none"
+
+function showoutput(message){
+    out.style.display="block";
+    out.innerText=message;
+}
 
 function clickHandler(){
     console.log("clicked")
-    var m=4;
+    
     
     var b=bill.value;
     var c=cash.value;
-    notes[1].innerText=b;
-    if(b>c){
-        var m="do you wanna get hit"
-    }
-    else if(c>b){
-        
-        calculatereturn (b,c)
+    if(b>0){
+         if(b>c){
+             var m="do you wanna get hit"
+         }
+         else if(c>b){
+
+             calculatereturn (b,c)
+         }
+         else{
+             showoutput("do you want to wash clothes")
+         }
+         var returnamount =c-b;
     }
     else{
-        var m="invalid bill amount"
+        console.log("Enter valid amount")
+        showoutput("invalid amount")
     }
-    var returnamount =c-b;
-    
 }
 
 function calculatereturn(bill , cash){
     var returnamount =cash-bill;
-    console.log(returnamount)
+   // console.log(returnamount)
 
-    for(let i=0;i<availableNotes.Length;i++){
-        var numberofNotes=Math.trunc(returnamount/availableNotes[i]);
+    for(let i=0;i<availableNotes.length;i++){
+        const numberofNotes=Math.trunc(returnamount/availableNotes[i]);
         
-        returnamount=returnamount%availableNotes[i];
-
-        console.log(returnamount)
-        notes[i].innerText=numberofNotes
+        returnamount = returnamount%availableNotes[i];
+     //   console.log(i)
+        console.log(numberofNotes)
+        notes[i].innerText = numberofNotes;
     }
 }
 
